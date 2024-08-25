12dddf0256275d0593d7680bd1f195430c0b35b7
John Schmidt <john@schmidthaus.rocks>
2024-08-24 21:40:43 -0700

    Using Render backend.

--- src/App.js
+++ src/App.js
@@ -69,7 +69,7 @@ class App extends Component {
         // for the Face Detect Mode: https://www.clarifai.com/models/face-detection
         // If that isn't working, then that means you will have to wait until their servers are back up. 
 
-        fetch('http://localhost:3000/imageurl', {
+        fetch('https://face-recognition-brain-api-bmhn.onrender.com/imageurl', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
@@ -79,7 +79,7 @@ class App extends Component {
             .then(response => response.json())
             .then(response => {
                 if (response) {
-                    fetch('http://localhost:3000/image', {
+                    fetch('https://face-recognition-brain-api-bmhn.onrender.com/image', {
                         method: 'put',
                         headers: {'Content-Type': 'application/json'},
                         body: JSON.stringify({
