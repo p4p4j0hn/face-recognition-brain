f294660af89d0ec32acf8cec3c1b93fcc645beb0
John Schmidt <john@schmidthaus.rocks>
2024-11-29 13:46:51 -0800

    feat: changed some css
    Signed-off-by: John Schmidt <john@schmidthaus.rocks>

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Signin.css";
 
 class Signin extends React.Component {
   constructor(props) {
@@ -39,7 +40,7 @@ class Signin extends React.Component {
     const { onRouteChange } = this.props;
     return (
       <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
-        <main className="pa4 black-80">
+        <main className="pa4">
           <div className="measure">
             <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
               <legend className="f1 fw6 ph0 mh0">Sign In</legend>
@@ -71,7 +72,7 @@ class Signin extends React.Component {
             <div className="">
               <input
                 onClick={this.onSubmitSignIn}
-                className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
+                className="b ph3 pv2 input-reset ba bg-transparent grow pointer f6 dib light-color"
                 type="submit"
                 value="Sign in"
               />
@@ -79,7 +80,7 @@ class Signin extends React.Component {
             <div className="lh-copy mt3">
               <p
                 onClick={() => onRouteChange("register")}
-                className="f6 link dim black db pointer"
+                className="f6 link dim db pointer"
               >
                 Register
               </p>
