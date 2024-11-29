54c4f11cdc6c903ae2b8aa2e67a1300a47865c26
John Schmidt <john@schmidthaus.rocks>
2024-11-29 15:54:46 -0800

    fix: formatting and css changes
    Signed-off-by: John Schmidt <john@schmidthaus.rocks>

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -15,7 +15,7 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
             onChange={onInputChange}
           />
           <button
-            className="w-30 grow f4 link ph3 pv2 dib white bg-blue"
+            className="w-30 grow f4 link ph3 pv2 dib image-submit-button"
             onClick={onButtonSubmit}
           >
             Detect
--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,16 +1,13 @@
-import React from 'react';
+import React from "react";
 
 const Rank = ({ name, entries }) => {
   return (
     <div>
-      <div className='white f3'>
-        {`${name}, your current entry count is...`}
-      </div>
-      <div className='white f1'>
-        {entries}
-      </div>
+      <div className="f3">{`${name}, your current entry count is...`}</div>
+      <div className="f1">{entries}</div>
     </div>
   );
-}
+};
 
 export default Rank;
+
