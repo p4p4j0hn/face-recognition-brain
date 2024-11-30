8328f8f8f14feb11c3ef670a13bf2fb49611779e
John Schmidt <john@schmidthaus.rocks>
2024-11-29 16:40:58 -0800

    fix: final css color scheme changes
    Signed-off-by: John Schmidt <john@schmidthaus.rocks>

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -10,7 +10,7 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
       <div className="center">
         <div className="form center pa4 br3 shadow-5">
           <input
-            className="f4 pa2 w-70 center"
+            className="f4 pa2 w-70 center light-background"
             type="text"
             onChange={onInputChange}
           />
